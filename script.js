9613da7c152164d4db0c2693592125ffcff1fae6
Barbara Szewczyk <comamysza@gmail.com>
2022-10-04 01:31:54 +0200

    Add goal cup fill function

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remaining = document.getElementById('remaining');
 
+updateGoalCup();
+
 smallCups.forEach((cup, index) => {
 	cup.addEventListener('click', () => {
 		fillCups(index);
@@ -24,4 +26,29 @@ function fillCups(idx) {
 			cup.classList.remove('full');
 		}
 	});
+
+	updateGoalCup();
+}
+
+function updateGoalCup() {
+	const fullCups = document.querySelectorAll('.full').length;
+
+	if (fullCups === 0) {
+		percentage.style.height = 0;
+		percentage.style.opacity = 0;
+		return;
+	}
+
+	// number of full cups * 250ml / 2 liters in percentage
+	const goalPercentage = ((fullCups * 0.25) / 2) * 100;
+	percentage.style.height = `${goalPercentage}%`;
+	percentage.innerText = `${goalPercentage}%`;
+	percentage.style.opacity = 1;
+
+	const left = 100 - goalPercentage;
+	if (left === 0) {
+		remaining.style.height = 0;
+		remaining.style.opacity = 0;
+	}
+	remaining.innerText = `${left}%`;
 }
